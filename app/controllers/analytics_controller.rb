70ff11ea33b28f473cb7b389d0f1cb74a1a15c0b
Chris Siku <chrissiku5@gmail.com>
2022-12-06 21:25:45 +0200

    Sort data to display

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,6 +1,14 @@
 class AnalyticsController < ApplicationController
-  def index
-    # Get all the analytics by the count
-    @analytics = Analytic.all.order(count: :desc)
+  def index   
+    ignore_list = ['what', 'the', 'is', 'a', 'to', 'of', 'and', 'in', 'for', 'on', 'that', 'by', 'this', 'with', 'i', 'who', 'how']
+    question_list = ['what', 'who', 'how', 'when', 'where', 'which', 'why']
+
+    @analytics = Analytic.all.order(count: :desc).reject do |analytic|
+      ignore_list.include?(analytic.word)
+    end
+
+    @questions = Analytic.all.order(count: :desc).select do |analytic|
+      question_list.include?(analytic.word)
+    end
   end
 end
